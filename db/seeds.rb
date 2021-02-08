d8c196ccbebdccd8a0bf3f589cd956bba97f8e68
aniicossio1997 <anacossio7@gmail.com>
2021-02-08 07:04:22 -0300

    retoque

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 user = User.create(email:"user@gmail.com",password:1234)
-(1..8).each do |i|
+id_nota=1
+(1..5).each do |i|
    book =Book.create(user:user,name:"book #{i}")
-   1.upto(rand(1..5)) do |j|
-     book.notes.create(title:"nota-#{j}",content: "- lista ", user:user)
+   1.upto(4) do |j|
+     book.notes.create(title:"nota #{id_nota} de #{book.name}",content: "- lista ", user:user)
+     id_nota+=1
    end
  end
- (1..4).each do |i|
-    Note.create(user:user,title:"nota-sin", content: "- nota sin cuaderno")
+ (1..4).each do |h|
+    Note.create(user:user,title:"nota-#{id_nota} global", content: "-sin cuaderno")
+    id_nota+=1
  end
     
